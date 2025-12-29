25690ddbab23bf7d215c6176f60288d6bc48268c
ShenLoong99 <yithuettsk29@gmail.com>
2025-12-30 00:49:36 +0800

    test fix

--- modules/auth/main.tf
+++ modules/auth/main.tf
@@ -47,7 +47,7 @@ resource "aws_cognito_identity_pool" "main" {
 
   cognito_identity_providers {
     client_id               = aws_cognito_user_pool_client.client.id
-    provider_name           = replace(aws_cognito_user_pool.pool.endpoint, "https://", "")
+    provider_name           = "cognito-idp.${var.aws_region}.amazonaws.com/${aws_cognito_user_pool.pool.id}"
     server_side_token_check = false
   }
 }
