b737d2e7a1a0b0b9d1f922d2ae9cb713ac28a81d
ShenLoong99 <yithuettsk29@gmail.com>
2025-12-30 00:06:59 +0800

    update cognito user pool client block

--- modules/auth/main.tf
+++ modules/auth/main.tf
@@ -24,14 +24,23 @@ resource "aws_cognito_user_pool" "pool" {
   }
 }
 
+# Cognito User Pool Client
 resource "aws_cognito_user_pool_client" "client" {
   name         = "bucket-list-client"
   user_pool_id = aws_cognito_user_pool.pool.id
+
+  generate_secret = false
 
   explicit_auth_flows = [
     "ALLOW_USER_SRP_AUTH",
     "ALLOW_REFRESH_TOKEN_AUTH"
   ]
+
+  allowed_oauth_flows_user_pool_client = true
+  allowed_oauth_flows = ["code"]
+  allowed_oauth_scopes = ["email", "openid", "profile"]
+
+  supported_identity_providers = ["COGNITO"]
 }
 
 // Grant Permissions
